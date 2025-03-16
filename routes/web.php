ef9714842ccbc73ba01ffb8822ebe43f7bf9ad4a
Mas-arief <autama715@gmail.com>
2025-03-16 17:13:01 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\LoginController;
+
+Route::get('/login', [LoginController::class, 'index']);
