065e04d377ad80a6be9e2e3d6a640a6f23917bd5
DiKyS77 <dikysw27@gmail.com>
2025-03-23 22:50:45 +0700

    diky

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/login', [LoginController::class, 'index']);
 use App\Http\Controllers\bagusController;
 
 Route::get('/bagus', [bagusController::class, 'tampilkan']);
+
+use App\Http\Controllers\dikyController;
+
+Route::get('/diky', [dikyController::class, 'tampilkan']);
